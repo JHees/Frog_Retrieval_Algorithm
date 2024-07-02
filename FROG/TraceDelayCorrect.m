0178a30c7bff6c4a0a9da2e3068f190e80278012
JHees <huang.xing.zhao@qq.com>
2024-07-02 22:18:01 +0800

    docs(traceDelayCorrect, test): 增加文档描述和测试流程

--- FROG/TraceDelayCorrect.m
+++ FROG/TraceDelayCorrect.m
@@ -1,16 +1,38 @@
-function [dt,Delay_time,Delay_N] = TraceDelayCorrect(I, D, F, iter)
-    [~, ND] = size(I);
+function [dt, Delay_time, Delay_N] = TraceDelayCorrect(I, D, F, iter)
+    % TraceDelayCorrect. This function estimates the delay per pixel in a double pulse FROG trace.
+    % 此函数用于估计双脉冲FROG trace中每像素的延迟时间。
+    % Only DOUBLE pulse!
+    % It performs iterative adjustments to refine the estimation of delay between two pulses and the corresponding pixel distance.
+    % Usage:
+    %     [dt, Delay_time, Delay_N] = TraceDelayCorrect(I, D, F, iter)
+    %
+    % Input:
+    %     I: Input FROG trace matrix (size: N x N)
+    %     D: Delay axis of the trace (can be a scalar or array)
+    %     F: Frequency axis of the trace
+    %     iter: Number of iterations to refine delay estimation
+    %
+    % Output:
+    %     dt: Estimated delay per pixel
+    %     Delay_time: Delay between two pulses calculated from SHG signal
+    %     Delay_N: Pixel distance related to the delay, derived from autocorrelation analysis
+    %
+    % Note:
+    %     The function uses both SHG signals and autocorrelation to derive the delays and iteratively adjusts the estimates.
+    %     The algorithm terminates when the change in estimated delays falls below a minimal threshold, ensuring precision.
+    %
+    % AUTHOR: Huang Xingzhao, July 02, 2024
+
     dt_res = zeros(1, iter + 1);
     if isscalar(D)
         dt_res(1) = D;
-        D = (-ND / 2:ND / 2 - 1)' * D;
     else
         dt_res(1) = D(2) - D(1);
     end
 
     for i = 1:iter
         shg = I(:, end / 2 + 1);
-        shg_ft = fftshift(ifft(shg, 'symmetric'));
+        shg_ft = fftshift(ifft(shg));
         p = mypeak(FTconvert(F), abs(shg_ft).^2);
         Delay_time = (abs(p(1) - p(2)) + abs(p(1) - p(3))) / 2;
 
@@ -29,6 +51,7 @@ function p = mypeak(x, y)
         y = x;
         x = 1:length(y);
     end
+    y = y / max(y);
     [~, pos_ind, ~, prominence] = findpeaks(y, "WidthReference", "halfheight", "MinPeakHeight", 0);
     [~, ind] = sort(prominence, 'descend');
     pos_ind = pos_ind(ind);
--- FROG/testSolveFrog.m
+++ FROG/testSolveFrog.m
@@ -2,31 +2,34 @@ clear
 % units: fs,nm,PHz
 c = 299.77;
 
-[F, T] = FTconvert((0:0.5:1500 - 0.25)');
-N = numel(T);
+[F, T] = FTconvert((0:1:5000 - 1)');
+N = floor(numel(T) / 2) * 2;
 FF = FTconvert(F);
-wavelength = 849.85;
-f0 = c / wavelength;
+wavelength = 850;
+f0 = 0;
 
-Delay = 300;
+Delay = 200;
 gauss = @(t0, tou)exp(- (T - t0).^2 ./ tou.^2 * 2 * log(2)) .* exp(1i * 2 * pi * (f0 .* T));
 
-A = [1, 0.75];
+A = [1, 1];
 t0 = [0, Delay];
-FWHM = [100, 150];
+FWHM = [100, 100];
 P = sum(gauss(t0, FWHM) .* A, 2);
+% P = flip(P);
 fm = 0.03;
 dm = 600;
 
-clear
-load('pulse_set.mat')
-f0 = 0;
-[F, T] = FTconvert(time' / 1e-15);
-P = pulse_set(3, :).';
-P = P ./ max(abs(P));
-P = flip(P);
-fm = 0.3;
-dm = 75;
+% clear
+% load('pulse_set.mat')
+% f0 = 0;
+% [F, T] = FTconvert(time' / 1e-15);
+% dT = T(2)-T(1);
+% dT = 1;
+% P = pulse_set(3, :).';
+% P = P ./ max(abs(P));
+% P = flip(P);
+% fm = 0.3;
+% dm = 75;
 
 [I_withoutNoise] = TraceGenerate(P);
 pulse_spectrum = abs(fftshift(fft(P))).^2;
@@ -37,7 +40,7 @@ Fc_sp = F(maxPsp);
 bitDepth = 16;
 maxLevel = 2^bitDepth -1;
 
-alpha = 0.3;
+alpha = 0.1;
 additiveNoiseLevel = alpha * maxLevel;
 
 if alpha == 0
@@ -46,43 +49,26 @@ else
     I = I_withoutNoise .* (1 + alpha * randn(N)) + alpha * poissrnd(additiveNoiseLevel, N) / additiveNoiseLevel;
     I(I < 0) = 0;
     I = round(I .* maxLevel) ./ maxLevel;
+    I(I>1)=1;
 end
-[I_withoutNoise, Do, Fo, Fc] = TraceResample(I_withoutNoise, T, F - 2 * Fc_sp, 128, 'eps', 1e-4);
+[I_withoutNoise, Do_wn, Fo_wn, Fc_wn] = TraceResample(I_withoutNoise, T, F - 2 * Fc_sp, 128, 'eps', 1e-4);
 figure(5)
-plotTrace(I_withoutNoise, Do, Fo);
-%% denoise and resample
-if alpha == 0
-    S_denoise = I;
-else
-    S_denoise = TraceDenoise(I, T, F, 5, 60, 0.5);
-end
-[S_resample, Do, Fo, Fc] = TraceResample(S_denoise, T, F - 2 * Fc_sp, 128, 'eps', 1e-4);
+plotTrace(I_withoutNoise, Do_wn, Fo_wn);
+%% Delay Correct
+dt = TraceDelayCorrect(I, 0.5, F, 50);
+%% denoise
+S_denoise = TraceDenoise(I, dt, F, 5, 600, 1);
+%% resample
+[S_resample, Do, Fo, Fc] = TraceResample(S_denoise, dt, F - 2 * Fc_sp, 128, 'eps', 1e-4);
 S = TraceMarginCorrect(S_resample, Do, Fo, Fc, F - Fc_sp, pulse_spectrum);
 
-figure(6)
-plotTrace(I, T, F);
+% figure(6)
+% plotTrace(I, T, F);
 figure(7)
-plotTrace(S, Do, Fo);
+plotTrace(S_resample, Do, Fo);
 %% Solve
-% [van] = solveFrog_vanilla(S, [], 2000, 1e-5, "over_correction", "k");
-% [gp] = solveFrog_GP(S, [], 1e-2, [50, 2000], 1e-5);
-% [scgp] = solveFrog_shortcutGP(S, [], 2e-3, 3, [50, 2000], 1e-5);
-% [pcgp] = solveFrog_PCGPA(S, [], [50, 2000], 1e-5); % 不收敛的话看看GP是否收敛，GP收敛的话延长GP初始化
 [pcgp_svd] = solveFrog_PCGPA(S, [], [2, 200], 1e-4, 'svd');
 [solver, ErrorBar] = TraceSolver(S, [], [50, 200], 5, 5, 1e-4);
-%%
-% figure(1)
-% plot(T, abs(P) / max(abs(P)), Do, [abs(van.P), abs(gp.P), abs(scgp.P), abs(pcgp.P), abs(pcgp_svd.P),abs(solver.P)])
-% legend(["origin", "vanilla", "gp", "scgp", "pcgp", "pcgp svd","solve"])
-% figure(2)
-% semilogy(...
-%     1:sum(van.iter), van.err, ...
-%     1:sum(gp.iter), gp.err, ...
-%     1:sum(scgp.iter), scgp.err, ...
-%     1:sum(pcgp.iter), pcgp.err, ...
-%     1:sum(pcgp_svd.iter), pcgp_svd.err, ...
-%     1:sum(solver.iter) + sum(solver2.iter), [solver.err, solver2.err])
-% legend(["vanilla", "gp", "scgp", "pcgp", "pcgp svd", "solve"])
 figure(1)
 plot(T, abs(P) / max(abs(P)), Do, [abs(pcgp_svd.P), abs(solver.P)])
 legend(["origin", "pcgp svd", "solve"])
@@ -91,14 +77,12 @@ semilogy( ...
     1:sum(pcgp_svd.iter), pcgp_svd.err, ...
     1:sum(solver.iter), solver.err)
 legend(["pcgp svd", "solve"])
-%%
+
 figure(3)
 clf
 plotPulse(T, P, [], dm, fm);
-% plotPulse(Do, pcgp_svd.P, [], dm, fm);
-% plotPulse(Do, pcgp_svd.G, dm, fm);
 plotPulse(Do, solver.P, ErrorBar, dm, fm);
-% plotPulse(Do, solver.G, dm, fm);
+plotPulse(Do, pcgp_svd.P, [], dm, fm);
 figure(8)
 retrievedTrace = TraceGenerate(solver.P, solver.P);
 plotTrace(retrievedTrace, Do, Fo);
@@ -127,3 +111,26 @@ realError = TraceError(I_withoutNoise, retrievedTrace)
 % plot(T,fftshift(At))
 % xlim([-dm,dm])
 % ylim([0,Inf])
+%% Solve and plot
+% [van] = solveFrog_vanilla(S, [], 2000, 1e-5, "over_correction", "k");
+% [gp] = solveFrog_GP(S, [], 1e-2, [50, 2000], 1e-5);
+% [scgp] = solveFrog_shortcutGP(S, [], 2e-3, 3, [50, 2000], 1e-5);
+% [pcgp] = solveFrog_PCGPA(S, [], [50, 2000], 1e-5); % 不收敛的话看看GP是否收敛，GP收敛的话延长GP初始化
+% figure(1)
+% plot(T, abs(P) / max(abs(P)), Do, [abs(van.P), abs(gp.P), abs(scgp.P), abs(pcgp.P), abs(pcgp_svd.P),abs(solver.P)])
+% legend(["origin", "vanilla", "gp", "scgp", "pcgp", "pcgp svd","solve"])
+% figure(2)
+% semilogy(...
+%     1:sum(van.iter), van.err, ...
+%     1:sum(gp.iter), gp.err, ...
+%     1:sum(scgp.iter), scgp.err, ...
+%     1:sum(pcgp.iter), pcgp.err, ...
+%     1:sum(pcgp_svd.iter), pcgp_svd.err, ...
+%     1:sum(solver.iter) + sum(solver2.iter), [solver.err, solver2.err])
+% legend(["vanilla", "gp", "scgp", "pcgp", "pcgp svd", "solve"])
+
+% figure(3)
+% clf
+% plotPulse(T, P, [], dm, fm);
+% plotPulse(Do, pcgp_svd.P, [], dm, fm);
+% plotPulse(Do, pcgp_svd.G, dm, fm);
