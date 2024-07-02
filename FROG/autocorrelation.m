a64a7272c370b058f43af28bc60cb71244e91ebe
JHees <huang.xing.zhao@qq.com>
2024-07-02 21:17:09 +0800

    fix(ac, mg):
    修改基线判定规则

--- FROG/autocorrelation.m
+++ FROG/autocorrelation.m
@@ -1,5 +1,6 @@
 function ac = autocorrelation(I)
+    n = round(size(I, 1) / 20);
     ac = sum(I, 1);
-    ac = ac - median(ac);
+    ac = ac - mean(ac([1:n, end - n:end]));
     ac = ac / max(ac);
 end
--- FROG/marginal.m
+++ FROG/marginal.m
@@ -1,5 +1,6 @@
 function mg = marginal(I)
+    n = round(size(I, 2) / 20);
     mg = sum(I, 2);
-    mg = mg - median(mg);
+    mg = mg - mean(mg([1:n, end - n:end]));
     mg = mg / max(mg);
 end
